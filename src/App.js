8f667b581ff6080d7568969f6c9b06c048861353
Alexsey215 <orlov200015@gmail.com>
2021-09-07 18:27:45 +0700

    'lesson-3'

--- src/App.js
+++ src/App.js
@@ -1,48 +1,115 @@
 import './App.scss';
+import {Form, Button, Container, Row, Col, ListGroup } from 'react-bootstrap';
 import { Message } from './components/Message/Message'
-import {useEffect, useState} from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
     const [messageList, setMessageList] = useState([]);
     const [value, setValue] = useState('');
-    const today = new Date();
-    const curTime = `${today.getHours()}:${today.getMinutes()}`;
+    const [chats, setChats] = useState([
+        {
+            name: 'Alex',
+            id: Date.now()
+        },
+        {
+            name: 'John',
+            id: Date.now()
+        },
+        {
+            name: 'Bob',
+            id: Date.now()
+        },
+        {
+            name: 'Julia',
+            id: Date.now()
+        }
+    ]);
+
+    const inputRef = useRef(null);
+
 
     useEffect(() => {
         if (messageList[messageList.length - 1]?.author ==='Me') {
-            setTimeout(() => {
-                setMessageList((prevMessageList) => [...prevMessageList,
+            const timeout = setTimeout(() => {
+                const today = new Date();
+                const curTime = `${today.getHours()}:${today.getMinutes()}`;
+                setMessageList((prevMessageList) => [
+                    ...prevMessageList,
                     {
-                        text: 'I am bot',
                         author: 'Bot',
+                        text: 'I am bot',
                         date: curTime,
                     }
                 ], [messageList])
             }, 1500)
+            return () => {
+                clearTimeout(timeout);
+            };
         }
+        inputRef.current.focus();
+
     }, [messageList])
-    const handleAddMessage = () => {
+    const handleAddMessage = (e) => {
+        e.preventDefault();
+        const today = new Date();
+        const curTime = `${today.getHours()}:${today.getMinutes()}`;
         setMessageList((prevMessageList) => [...prevMessageList, {
             author: 'Me',
             text: value,
             date: curTime,
         }])
+        setValue("");
+        inputRef.current.focus();
     }
     const handleChange = (e) => {
         setValue(e.target.value);
     }
     return (
-        <div className="App chat-container">
-            <input className="App-input" value={value} onChange={handleChange} />
-            <button className="App-btn" onClick={handleAddMessage}>Send message</button>
-            {messageList.map((messageData, i) =>
-                <Message
-                    key={i}
-                    messageAuthor={messageData.author}
-                    messageText={messageData.text}
-                    renderCurDate={messageData.date}
-                />)}
-        </div>
+        <Container className="App mt-2" fluid="md">
+            <Row>
+                <Col xs={4}>
+                    <ListGroup>
+                        <ListGroup.Item action  className="text-primary">
+                            {chats[0].name}
+                        </ListGroup.Item>
+
+                        <ListGroup.Item action  className="text-primary">
+                            {chats[1].name}
+                        </ListGroup.Item>
+
+                        <ListGroup.Item action  className="text-primary">
+                            {chats[2].name}
+                        </ListGroup.Item>
+
+                        <ListGroup.Item action  className="text-primary">
+                            {chats[3].name}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col  xs={8}>
+                    <Form>
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Message"
+                            value={value}
+                            onChange={handleChange}
+                            ref={inputRef}
+                            className="text-primary"
+                        />
+                        <Button onClick={handleAddMessage} variant="outline-primary" className="mt-2">Send message</Button>
+                    </Form>
+
+                {messageList.map((messageData, i) =>
+                    <Message
+                        key={i}
+                        messageAuthor={messageData.author}
+                        messageText={messageData.text}
+                        renderCurDate={messageData.date}
+                    />)}
+                </Col>
+            </Row>
+
+        </Container>
     );
 }
 
